52b03813961797948885c03befe6bd5467bb0f65
Rohan Swaby <rohan_swaby51@yahoo.com>
2021-08-09 23:26:21 -0400

    add cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect } from 'react';
+import RestApiClient from './RestApiClient';
 
+const api = new RestApiClient();
 function App() {
+  useEffect(() => {
+    console.log('testing cors with useEffect');
+    api.get('courses/')
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div className="App">
       <div>frontend</div>
