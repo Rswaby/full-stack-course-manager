1f82fd7c2f87e4d5599dff5db6601c90adb8a726
Rohan Swaby <rohan_swaby51@yahoo.com>
2021-08-25 10:47:35 -0400

    display courses

--- client/src/components/CourseDetails.js
+++ client/src/components/CourseDetails.js
@@ -3,7 +3,7 @@ import React from 'react';
 function CourseDetails() {
   return (
     <div className="wrap main--grid">
-      <div>List All Courses Here</div>
+      <div>course Details </div>
     </div>
   );
 }
--- client/src/components/CourseList.js
+++ client/src/components/CourseList.js
@@ -1,17 +1,44 @@
-import React, { useContext, useEffect } from 'react';
+import React, {
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { Link } from 'react-router-dom';
+import { uniqueId } from 'lodash';
 import { Context } from '../context';
 
 function CourseList() {
   const { restClient } = useContext(Context);
+  const [courses, setCourses] = useState([]);
+  const [addNewCourseId] = useState(uniqueId('newCourse'));
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     restClient.getAllCourses()
       .then((data) => {
-        console.log(data);
+        setCourses(data);
+        setIsLoading(false);
       });
   }, []);
+  const displayCourse = () => {
+    let coursedata = [];
+    coursedata = courses.map((course) => (
+      <Link key={course.id} className="course--module course--link" to={`/courses/${course.id}`}>
+        <h2 className="course--label">Course</h2>
+        <h3 className="course--title">{course.title}</h3>
+      </Link>
+    ));
+    coursedata.push(
+      <Link key={addNewCourseId} className="course--module course--add--module" to="/courses/create">
+        ＋ New Course
+      </Link>,
+    );
+    return coursedata;
+  };
+  console.log(courses);
+  console.log(isLoading);
   return (
     <div className="wrap main--grid">
-      <div>List All Courses Here</div>
+      { !isLoading ? (displayCourse()) : (<p>loading..</p>)}
     </div>
   );
 }
